f6d83f8e0ad6a54f93851ba5f786a8c259b88de7
patjacq-commits <patjacq@skynet.be>
2026-06-11 18:04:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // v1.6.1 : Notification push quotidienne à 7h00
 // ============================================================
 
-const CACHE_NAME = 'mcr-flotte-v260611.1745';
+const CACHE_NAME = 'mcr-flotte-v260611.1803';
 const CACHE_URLS = ['./', './index.html', './icon-192.png', './manifest.json'];
 
 // Rappels RV en mémoire { id, titre, corps, tag, fireAt }
