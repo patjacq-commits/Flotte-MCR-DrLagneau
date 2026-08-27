1c39f9ddb8c178ba560325a0561b06bddbe438c1
patjacq-commits <patjacq@skynet.be>
2026-08-27 14:30:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // STRATÉGIE : HTML toujours depuis le réseau, assets en cache
 // ============================================================
 
-const CACHE_NAME = 'mcr-flotte-v260827.1415';
+const CACHE_NAME = 'mcr-flotte-v260827.1430';
 const CACHE_ASSETS = ['./icon-192.png', './manifest.json', './index.html'];
 
 // ── Installation ──────────────────────────────────────────────
